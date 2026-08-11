3d60e7efd177f84ecf3e3e219b7dc31d6c2cce7a
hy68483 <hys2013789@gmail.com>
2026-08-11 23:15:32 +0900

    Revert "Merge branch 'feature/consents-measurement-sessions' into develop"
    This reverts commit 598e46cf60236121755faf8c33da692a4f46152a, reversing
    changes made to acc4b036b44039afc8109ed7c09391a47252274f.

--- hackathon/backend/app/repositories/__init__.py
+++ hackathon/backend/app/repositories/__init__.py
@@ -1,14 +1,5 @@
-from app.repositories.consent_repository import ConsentRepository
 from app.repositories.foot_profile_repository import FootProfileRepository
-from app.repositories.measurement_repository import MeasurementRepository
 from app.repositories.product_repository import BrandRepository, ProductRepository
 from app.repositories.user_repository import UserRepository
 
-__all__ = [
-    "BrandRepository",
-    "ConsentRepository",
-    "FootProfileRepository",
-    "MeasurementRepository",
-    "ProductRepository",
-    "UserRepository",
-]
+__all__ = ["BrandRepository", "FootProfileRepository", "ProductRepository", "UserRepository"]
--- hackathon/backend/app/services/__init__.py
+++ hackathon/backend/app/services/__init__.py
@@ -1,14 +1,5 @@
 from app.services.auth_service import AuthService
-from app.services.consent_service import ConsentService
-from app.services.measurement_session_service import MeasurementSessionService
 from app.services.profile_service import ProfileService
 from app.services.product_service import BrandService, ProductService
 
-__all__ = [
-    "AuthService",
-    "BrandService",
-    "ConsentService",
-    "MeasurementSessionService",
-    "ProductService",
-    "ProfileService",
-]
+__all__ = ["AuthService", "BrandService", "ProductService", "ProfileService"]
